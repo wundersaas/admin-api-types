87ee00c99d72849441f1625768a1c57ee0f15ac3
Maicol Bruski <maicolbruski@gmail.com>
2024-07-04 18:25:53 -0300

    location d

--- src/inventory.ts
+++ src/inventory.ts
@@ -1,10 +1,3 @@
-import { ImageDTO } from 'admin-api-types'
-
-type InventoryLevel = {
-  location_id: string
-  available: string
-}
-
 export interface InventoryDTO {
   id: string
   variant_id: string
@@ -14,8 +7,6 @@ export interface InventoryDTO {
   updated_at: Date
   title: string
   sku: string
-  image: ImageDTO
-  inventory_levels: InventoryLevel[]
   // reserved_quantity: string
   product: {
     id: string
---
+++ src/location.ts
@@ -0,0 +1,18 @@
+export interface LocationDTO {
+  id: string
+  created_at: string
+  updated_at: string
+  active: string
+  address1: string
+  address2: string
+  neighbourhood: string
+  number: string
+  city: string
+  country: string
+  country_code: string
+  title: string
+  phone: string
+  province: string
+  province_code: string
+  zipcode: string
+}
