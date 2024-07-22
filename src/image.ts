bc1286b1c1860cef8f6409fe70cc4080f6ffcb38
Maicol Bruski <maicolbruski@gmail.com>
2024-07-21 23:08:25 -0300

    image

--- src/image.ts
+++ src/image.ts
@@ -1,11 +1,11 @@
 export type ImageDTO = {
   src: string
   alt?: string
-  created_at?: string
   updated_at?: string
   created_at?: string
   position?: number
   width?: number
   height?: number
   type?: string
+  bytes?: number
 }
