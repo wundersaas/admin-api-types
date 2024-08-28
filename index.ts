671c5dd2d02d63c51bc7ae08d0d74eedab5b6f50
Maicol Bruski <maicolbruski@gmail.com>
2024-08-28 16:24:37 -0300

    user input

--- index.ts
+++ index.ts
@@ -7,3 +7,4 @@ export {
 } from './src/category'
 export { InventoryItemInput, InventoryItemOutput } from './src/inventory-item'
 export { LocationInput, LocationOutput } from './src/location'
+export { UserInput, UserOutput } from './src/user'
